6b8eddaa45f9fd0fcab8d8fc8a0bc468aa847dc7
Valentin <valentyn.patsera@gmail.com>
2021-08-18 15:18:43 +0300

    code refactoring

--- static/ui.js
+++ static/ui.js
@@ -1,11 +1,22 @@
-export class UiString extends String {
-  constructor(value = "") {
-    super(value);
-  }
-  span(value, className = "") {
-    return new UiString(this + `<span class=${className}>${value}</span>`);
-  }
-  li(value, className = "") {
-    return new UiString(this + `<li class=${className}>${value}</li>`);
-  }
-}
+const leftCol = document.getElementById("left-col");
+const rightCol = document.getElementById("right-col");
+const intervalCol = document.getElementById("interval-col");
+const startButton = document.getElementById("start-button");
+const concModeSwitch = document.getElementById("conc-mode-switch");
+
+export const ui = {
+  cols: {
+    left: leftCol,
+    right: rightCol,
+    interval: intervalCol,
+    clearColumns: () => {
+      leftCol.innerHTML = "";
+      rightCol.innerHTML = "";
+      intervalCol.innerHTML = "";
+    },
+  },
+  buttons: { start: startButton },
+  switches: {
+    concMode: concModeSwitch,
+  },
+};
